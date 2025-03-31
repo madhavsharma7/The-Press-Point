3320f32fcead83b5467888c0a9af114526dca930
Madhav Sharma <madhav032001@gmail.com>
2025-04-01 01:25:12 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 import Home from "./Home/index"
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div id="container">
       <Home />
     </div>
+
+    // <BrowserRouter>
+    //   <div className='App'>
+    //     <Routes>
+    //       <Route  element={<Home />} />
+      
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
+
+
   );
 }
 
