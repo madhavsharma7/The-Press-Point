322dba8a3d6e4be3509721baf1db79ab76ec53f7
Madhav Sharma <madhav032001@gmail.com>
2025-04-11 01:45:22 +0530

    done

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const User = require('./models/User'); // ✅ CORRECT
-
-
+const User = require('../Backend/models/User'); // ✅ CORRECT
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
