a0edeecdbf9ec5d6dc129f77c8594815df15b1fb
Madhav Sharma <madhav032001@gmail.com>
2025-04-25 23:28:52 +0530

    done

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const axios = require('axios');
 const User = require('./User');
 
