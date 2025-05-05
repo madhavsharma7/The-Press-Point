062594840744af554e62da9eca43c5905c710f3c
Madhav Sharma <madhav032001@gmail.com>
2025-05-05 22:29:56 +0530

    removed axios

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const User = require("./User");
-const axios = require("axios"); // Needed for HTTP requests
+// const axios = require("axios"); // Needed for HTTP requests
 const jwt = require("jsonwebtoken");
 
 dotenv.config();
